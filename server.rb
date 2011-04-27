c65c8f3ea071627ce3edd97ad2db7a241a94dc78
John Pacheco <pacheco@champlain.ai.sri.com>
2011-04-27 12:35:58 -0700

    Updated proxy server with UUID-based logging

--- server.rb
+++ server.rb
@@ -10,6 +10,22 @@ AURA_ENDPOINT = "http://localhost:8000/SOAP"
 aura = Aura.instance
 # If the client says the connection was closed then there was probably an exception suppressed by this mechanism.
 set :show_exceptions => false
+set :logging, true
+
+before do
+	id = params[:uuid]
+	id = id ? id : "oldAppVersion"
+	log = File.open("Inquire-log-" + id + ".log", "a")
+	log.puts "\n\n\n===========================================\n\n\n"
+	log.puts "REQUEST: " + request.path() 
+	log.puts Time.now
+	log.puts "IP Address: " + request.ip
+	log.puts "parameters"
+	#log.puts params
+	params.each_pair{|key, value| log.puts "#{key}:\n #{value}"}
+	log.close
+	request 
+end
 
 post "/answers" do
   begin
@@ -22,36 +38,48 @@ post "/answers" do
     aura.ask_question(params[:question])
     if aura.valid_question?
       status 200
-      aura.answer_question
+      logResult(aura.answer_question)
     else
       status 400
-      aura.question_errors
+      logResult(aura.question_errors)
     end
   rescue StandardError => ex
     status 400
-    "An error occurred: " + ex.class.to_s + "<br /><br />" + ex.message
+    logResult("An error occurred: " + ex.class.to_s + "<br /><br />" + ex.message)
   end
 end
 
 post "/suggested_questions_lists" do
-  aura.suggest_questions(params)
+  logResult(aura.suggest_questions(params))
 end
 
 post "/structured_questions_lists" do
-  aura.get_structured_questions(params)
+  logResult(aura.get_structured_questions(params))
 end
 
 post "/suggested_formatted_questions_lists" do
-  aura.suggest_formatted_questions(params)
+  logResult(aura.suggest_formatted_questions(params))
 end
 
 post "/formatted_structured_questions_lists" do
-  aura.get_formatted_structured_questions(params)
+  logResult(aura.get_formatted_structured_questions(params))
 end
 
 get "*" do
-  puts "#{params[:splat]} 404 not found"
+  logResult(puts "#{params[:splat]} 404 not found")
   pass
+end
+
+def logResult(result)
+	id = params[:uuid]
+	id = id ? id : "oldAppVersion"
+	log = File.open("Inquire-log-" + id + ".log", "a")
+	log.puts "*******************************************"
+	log.puts "RESPONSE: "
+	log.puts Time.now
+	log.puts result
+	log.close
+	result 
 end
 
 def wait
